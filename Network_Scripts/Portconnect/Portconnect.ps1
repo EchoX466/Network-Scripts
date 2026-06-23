bcb3a1aa2c44262c688b2746e4b68f577891f067
EchoX466 <Roscoe.harmon@gmail.com>
2026-06-23 15:24:46 -0700

    code improv: Split the list of IP's and Ports - Testing needed

--- Network_Scripts/Portconnect/Portconnect.ps1
+++ Network_Scripts/Portconnect/Portconnect.ps1
@@ -5,7 +5,7 @@ $IPList = Read-Host -Prompt "Please enter the list of IP's you want to test: "
 
 # Split IPList 
 
-# IP's = $IPList -split ....
+IP's = $IPList -split "," | ForEach-Object { $_.Trim() } | Where-Object {$_ -ne "" }
 
 # Accept a list of Ports
 
@@ -13,4 +13,6 @@ $PortList = Read-Host -Prompt "Please enter the list of ports you want to test: 
 
 # Split PortList
 
-# Ports = $PortList -split ',' ....
+# $Ports = $PortList -split "," | ForEach-Object { $_.Trim() } | Where-Object { $_ -ne "" }
+
+
